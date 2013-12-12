7937e75ed67f9a31170750392b7e31f414a2f517
Ben Cook <jbenjamincook@gmail.com>
2013-12-12 16:59:02 -0500

    for cleaning the netflix data

---
+++ data-cleaning.py
@@ -0,0 +1,47 @@
+##############################################
+## This program cleans Netflix data and writes
+## it to a CSV file.
+## 
+## Usage: python clean_data.py <path to movies> <number of movies to load>
+## 
+## Note:  to load all the movies in the directory use 'all' for number
+##        for number of movies. e.g.
+##       
+##        python clean_data.py download/training_set all
+##
+## 
+##############################################
+
+import numpy as np
+import pandas as pd
+import os
+from sys import argv
+
+#if __name__ == 'main':
+path = argv[1]
+num_movies = argv[2]
+
+if num_movies == 'all':
+	files = os.listdir(path)
+else:
+	files = os.listdir(path)[:int(num_movies)]
+
+data = {}
+
+ct = 0
+for f in files:
+	if ct % 100 == 0:
+		print ct
+	infile = open('%s/%s' % (path, f), 'r')
+	movie = infile.readline().strip()[:-1]
+	data[movie] = {}
+	for line in infile:
+		user,rating,_ = line.split(',')
+		data[movie][user] = rating
+	infile.close()
+	ct += 1
+
+outfile = 'data_%s.csv' % num_movies
+pd.DataFrame(data).to_csv(outfile)
+
+
