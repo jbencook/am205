66929a319c08da3fc12dd2f346fd334a11e6dba8
Ben Cook <jbenjamincook@gmail.com>
2013-12-13 09:08:20 -0500

    svd

---
+++ svd.py
@@ -0,0 +1,13 @@
+import numpy as np
+from scipy.io import mmwrite
+from scipy.sparse import lil_matrix
+import pandas as pd
+import os
+from sys import argv
+
+'''
+Let's try some SVD!
+'''
+
+if __name__ == '__main__':
+	print 'hello world!'
