5c433df7d164d5d5fbe1216067f2cd532335c8cd
Ben Cook <jbenjamincook@gmail.com>
2013-12-13 09:06:56 -0500

    500 movie dataset

--- utils/data-cleaning.py
+++ utils/data-cleaning.py
@@ -11,6 +11,24 @@ This program cleans Netflix data and writes it to a CSV file.
 Usage: python data-cleaning.py <path to movies> <number of movies to load>
 '''
 
+def write_user_ids(path, files, outfile):
+	users = set()
+	i = 0
+	of = open(outfile, 'w')
+	for ndx,f in enumerate(files):
+		if ndx % 100 == 0:
+			print ndx
+		infile = open('%s/%s' % (path, f), 'r')
+		infile.readline().strip()[:-1]
+		for line in infile:
+			id, _, _ = line.split(',')
+			if id not in users:
+				users.add(id)
+				of.write('%d,%s\n' % (i, id))
+				i += 1
+	print '%d total users' % (i + 1)
+	of.close()
+
 def get_user_dict(fname):
 	f = open(fname, 'r')
 	user_dict = {}
@@ -23,14 +41,15 @@ def get_user_dict(fname):
 if __name__ == '__main__':
 	path       = argv[1]
 	num_movies = argv[2]
-	#user_file  = argv[3]
-	user_file = 'user_ids.csv'
+	user_file  = argv[3]
+	#user_file = 'user_ids.csv'
 
 	if num_movies == 'all':
 		files = os.listdir(path)
 	else:
 		files = os.listdir(path)[:int(num_movies)]
 
+	write_user_ids(path, files, user_file)
 	users = get_user_dict(user_file)
 	data  = lil_matrix((len(users), len(files)))
 
